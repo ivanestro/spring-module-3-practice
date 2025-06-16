56dc59714d371178c26dc8a2c24b34c0463f69ba
ivanestro <iestropigan@rrc.ca>
2025-06-16 09:55:35 -0500

    Update testing os systems and while loops

--- Assignment_3.py
+++ Assignment_3.py
@@ -1,4 +1,6 @@
+import os
 import random
+from time import sleep
 
 # Collections 
 menu_options = {"Deposit": "D", "Withdraw": "W", "Quit": "Q"}
@@ -6,49 +8,53 @@ user_options = ""
 
 # random balance
 balance = random.uniform(1000, 10000)
-text_header = "*" * 40
-text_financial = "PIXELL RIVER FINANCIAL".center(40)
-text_simulator = "ATM Simulator".center(40)
-text_balance = f"Your current balance is: ${balance:.2f}".center(40)
 
-text_deposit = "Deposit: D".center(40)
-text_withdraw = "Withdraw: W".center(40)
-text_quit = "Quit: Q".center(40)
-text_invalid = "INVALID SELECTION".center(40)
-text_insufficient_funds = "INSUFFICIENT FUNDS".center(40)
 
-print(f"{text_header}"
-      f"\n{text_financial}"
-      f"\n{text_simulator}\n"
-      f"\n{text_balance}\n"
-      f"\n{text_deposit}"
-      f"\n{text_withdraw}"
-      f"\n{text_quit}"
-      f"\n{text_header}")
-
-user_options = input("Enter your selection ").upper()
-
-if user_options not in menu_options.values(): 
+while user_options != "Q":
     
-    print(f"\n{text_header}"
-          f"\n{text_invalid}"
+    text_header = "*" * 40
+    text_financial = "PIXELL RIVER FINANCIAL".center(40)
+    text_simulator = "ATM Simulator".center(40)
+    text_balance = f"Your current balance is: ${balance:.2f}".center(40)
+
+    text_deposit = "Deposit: D".center(40)
+    text_withdraw = "Withdraw: W".center(40)
+    text_quit = "Quit: Q".center(40)
+    text_invalid = "INVALID SELECTION".center(40)
+    text_insufficient_funds = "INSUFFICIENT FUNDS".center(40)
+
+    
+    print(f"{text_header}"
+          f"\n{text_financial}"
+          f"\n{text_simulator}\n"
+          f"\n{text_balance}\n"
+          f"\n{text_deposit}"
+          f"\n{text_withdraw}"
+          f"\n{text_quit}"
           f"\n{text_header}")
     
-if user_options == "D":
-    money_increase = float(input("Enter your transaction amount: "))
-    balance += money_increase
-    print(f"Your current balance is: ${balance:.2f}")
-
-elif user_options == "W":
-    money_increase = float(input("Enter your transaction amount: "))
-    if money_increase > balance:
+    user_options = input("Enter your selection ").upper()
+    
+    if user_options not in menu_options.values(): 
         print(f"\n{text_header}"
-              f"\n{text_insufficient_funds}"
+              f"\n{text_invalid}"
               f"\n{text_header}")
-    else:
-        balance -= money_increase
-        print(f"\n{text_header}"
-              f"\nYour current balance is: {balance:.2f}"
-              f"\n{text_header}")
+        
+    elif user_options == "D":
+            money_increase = float(input("Enter your transaction amount: "))
+            balance += money_increase
+            
+    elif user_options == "W":
+        money_increase = float(input("Enter your transaction amount: "))
+        
+        if money_increase > balance:
+              print(f"\n{text_header}"
+                    f"\n{text_insufficient_funds}"
+                    f"\n{text_header}")
+        else:
+            balance -= money_increase
+            print(f"\n{text_header}")
 
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+print("Program is shutting down")
